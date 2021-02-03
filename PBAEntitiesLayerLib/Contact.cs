59b3805323201ea93f30ccf22b5789a745271f99
chalap <69064647+chalap@users.noreply.github.com>
2021-02-03 21:33:59 +0530

    Add files via upload

---
+++ PBAEntitiesLayerLib/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBAEntitiesLayerLib
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public int CategoryId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+        public long PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
