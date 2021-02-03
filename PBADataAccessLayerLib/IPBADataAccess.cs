a9fa6e364025fab1aa9d96b34f0be44849f55edd
chalap <69064647+chalap@users.noreply.github.com>
2021-02-03 21:32:18 +0530

    Add files via upload

---
+++ PBADataAccessLayerLib/IPBADataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBAEntitiesLayerLib;
+using PBAExceptionLib;
+
+namespace PBADataAccessLayerLib
+{
+    public interface IPBADataAccess
+    {
+        List<ContactCategory> GetAllcategoryNames();
+        void AddContactCategory(ContactCategory cat);
+        List<Contact> GetAllContacstById(int id);
+        void AddContact(Contact contact);
+        void DeleteContactById(int id);
+        void DeleteCategoryById(int id);
+        void UpdateContactById(Contact contact);
+        void UpdateCategoryById(ContactCategory cat);
+    }
+}
